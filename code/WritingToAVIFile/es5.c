923175c45e5f17fc5d7cc49e9ca6562ce3a51695
Andrea Giulianelli <andrygiulio@gmail.com>
2019-04-11 09:11:57 +0200

    Update code

--- code/WritingToAVIFile/es5.c
+++ code/WritingToAVIFile/es5.c
@@ -23,6 +23,28 @@ int main(int argc,char** argv)
         return -1;
     }
 
-    IplImage* bgr_frame = cvQueryFrame(capture); //Inizia la lettura del video
-    
+    //Inizia la lettura del video
+    //Necessario per settare le proprietà del video nella struttura
+    IplImage* bgr_frame = cvQueryFrame(capture); 
+    double fps = cvGetCaptureProperty(capture,CV_CAP_PROP_FPS);
+    CvSize size = cvSize(
+        (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH),
+        (int)cvGetCaptureProperty(capture,CV_CAP_PROP_FRAME_HEIGHT)
+        );
+
+    //Il secondo parametro è il video codec (Tramite CV_FOURCC riusciamo ad esprimerlo in quattro caratteri)    
+    CvVideoWriter* writer = cvCreateVideoWriter(argv[2],CV_FOURCC('M','J','P','G'),fps,size);
+    IplImage* logpolar_frame = cvCreateImage(size,IPL_DEPTH_8U,3);
+
+    while((bgr_frame=cvQueryFrame(capture))!=NULL)
+    {
+        cvLogPolar(bgr_frame,logpolar_frame,cvPoint2D32f(bgr_frame->width/2,bgr_frame->height/2),40,CV_INTER_LINEAR+CV_WARP_FILL_OUTLIERS);
+        cvWriteFrame(writer,logpolar_frame);
+    }
+
+    cvReleaseVideoWriter(&writer);
+    cvReleaseImage(&logpolar_frame);
+    cvReleaseCapture(&capture);
+    return(0);
+
 }
