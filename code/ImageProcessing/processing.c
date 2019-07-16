0a1895546cdd7a338c78543c15616f45058e0cda
Andrea Giulianelli <andrygiulio@gmail.com>
2019-07-16 18:07:31 +0200

    Update code

--- code/ImageProcessing/processing.c
+++ code/ImageProcessing/processing.c
@@ -10,7 +10,23 @@ int main(int argc,char** argv)
         exit(1);
     }
     IplImage* img = cvLoadImage(argv[1]);
+
     //SMOOTHING
+    /*
+        void cvSmooth(
+            const CvArr*    src,
+            CvArr**         dst,
+            int smoothtype = CV_GAUSSIAN
+            int param1 = 3
+            int param2 = 0
+            int param3 = 0
+            int param4 = 0
+        );
+
+        Il valore dei param1/2/3/4 dipende dal smoothtype selezionato
+        Tabella a pagina 126
+        
+    */
     
     return 0;
 }
