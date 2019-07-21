d587af8a7139be17e2c75b695309a9245429d561
Andrea Giulianelli <andrygiulio@gmail.com>
2019-07-21 20:25:06 +0200

    Update code

--- code/ImageProcessing/processing.c
+++ code/ImageProcessing/processing.c
@@ -13,6 +13,10 @@ int main(int argc,char** argv)
 
     //SMOOTHING
     /*
+        Lo smoothing viene utilizzato per ridurre il rumore o gli errori della camera
+
+    */
+    /*
         void cvSmooth(
             const CvArr*    src,
             CvArr**         dst,
@@ -26,7 +30,55 @@ int main(int argc,char** argv)
         Il valore dei param1/2/3/4 dipende dal smoothtype selezionato
         Tabella a pagina 126
         
+        IMPORTANTE:
+        Il filtro di Gauss non mantiene i bordi.
+        Il filtro bilaterale invece li mantiene.
     */
+
+   /*
+    IMAGE MORPHOLOGY
+
+    -Dilatation and Erosion
+        https://www.youtube.com/watch?v=7-FZBgrW4RE
+        https://www.youtube.com/watch?v=HcpxKCdzjdY (Mostra come utilizzare gli structuring element)
+        La dilatazione, come dice il nome provvede ad aggiungere pixel nei bordi di un immagine
+        in modo da ingrandirla o addensarla
+        L'erosione, riduce il numero di pixel dai bordi dell'immagine per contrarre l'immagine e farla più "fine" 
+
+        void cvErode(
+            IplImage* src,
+            IplImage* dst,
+            IplConvKernel* B=NULL
+            int iterations = 1
+        )
+
+        void cvDilate(
+            IplImage* src
+            IplImage* dst
+            IplConvKernel* B=NULL
+            int iterations = 1
+        )
+
+        Quando B (il kernel, o structuring element) non viene passato, è di default un kernel 3x3 con un ancora centrale
+
+    
+    -Creare un proprio kernel
+        Il kernel ha un tipo: IplConvKernel
+        Inoltre i kernel sono allocati con cvCreateStructuringElementEx() 
+        e deallocati con cvReleaseStructuringElement()
+
+        IplConvKernel* cvCreateStructuringElementEx(
+            int cols,
+            int rows,
+            int anchor_x,
+            int anchor_y,
+            int shape,
+            int* values = NULL)
+        (PAG. 135)
+
+        
+
+   */
     
     return 0;
 }
